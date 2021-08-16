f87ed423ac4e4ec3aef9a67a5992315210b82368
salybu <rkdmsdl93@gmail.com>
2021-08-16 21:03:02 +0900

    [CHORE] style 세팅

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import theme from 'styles/theme';
+import GlobalStyles from 'styles/GlobalStyles';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
