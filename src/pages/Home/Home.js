400697e0da3ff3faac8cbf092c9b1db2213932c8
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2021-08-17 14:52:04 +0900

    [Style] 기본 레이아웃용 박스 추가 및 시작 버튼, input 하나 추가

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,175 @@
-import React from "react";
-
+import React from 'react';
+import styled, { keyframes } from 'styled-components';
 
 const Home = () => {
-  return (<div>Home</div>)
-}
+  return (
+    <SortingMachineBox>
+      <form>
+        <InputField>
+          <input type='text' name='numbers' required />
+          <label htmlFor='numbers'>숫자 입력</label>
+        </InputField>
+        <StartButton>
+          <span></span>
+          <span></span>
+          <span></span>
+          <span></span>
+          시작
+        </StartButton>
+      </form>
+    </SortingMachineBox>
+  );
+};
 
-export default Home; 
+const SortingMachineBox = styled.div`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  width: 480px;
+  padding: 40px;
+  transform: translate(-50%, -50%);
+  background: rgba(0, 0, 0, 0.5);
+  box-sizing: border-box;
+  box-shadow: 0 15px 25px rgba(0, 0, 0, 0.6);
+  border-radius: 10px;
+`;
+
+const InputField = styled.div`
+  position: relative;
+
+  input {
+    width: 100%;
+    padding: 10px 0;
+    font-size: 16px;
+    color: #fff;
+    margin-bottom: 30px;
+    border: none;
+    border-bottom: 1px solid #fff;
+    outline: none;
+    background: transparent;
+  }
+
+  label {
+    position: absolute;
+    top: 0;
+    left: 0;
+    padding: 10px 0;
+    font-size: 16px;
+    color: #fff;
+    pointer-events: none;
+    transition: 0.5s;
+  }
+
+  input:valid ~ label,
+  input:focus ~ label {
+    top: -20px;
+    left: 0;
+    color: #03e9f4;
+    font-size: 12px;
+  }
+`;
+
+const btnAnimation1 = keyframes`
+  0% {
+    left: -100%;
+  }
+  50%,100% {
+    left: 100%;
+  }
+`;
+
+const btnAnimation2 = keyframes`
+  0% {
+    top: -100%;
+  }
+  50%,100% {
+    top: 100%;
+  }
+`;
+
+const btnAnimation3 = keyframes`
+  0% {
+    right: -100%;
+  }
+  50%,100% {
+    right: 100%;
+  }
+`;
+
+const btnAnimation4 = keyframes`
+  0% {
+    bottom: -100%;
+  }
+  50%,100% {
+    bottom: 100%;
+  }
+`;
+
+const StartButton = styled.button`
+  background: transparent;
+  border: none;
+  position: relative;
+  display: inline-block;
+  padding: 10px 20px;
+  color: #03e9f4;
+  font-size: 16px;
+  text-decoration: none;
+  text-transform: uppercase;
+  overflow: hidden;
+  transition: 0.5s;
+  margin-top: 40px;
+  letter-spacing: 4px;
+
+  &:hover {
+    background: #03e9f4;
+    color: #fff;
+    border-radius: 5px;
+    box-shadow: 0 0 5px #03e9f4, 0 0 25px #03e9f4, 0 0 50px #03e9f4, 0 0 100px #03e9f4;
+  }
+
+  & > span {
+    position: absolute;
+    display: block;
+  }
+
+  & > span:nth-child(1) {
+    top: 0;
+    left: -100%;
+    width: 100%;
+    height: 2px;
+    background: linear-gradient(90deg, transparent, #03e9f4);
+    animation: ${btnAnimation1} 1s linear infinite;
+  }
+
+  & > span:nth-child(2) {
+    top: -100%;
+    right: 0;
+    width: 2px;
+    height: 100%;
+    background: linear-gradient(180deg, transparent, #03e9f4);
+    animation: ${btnAnimation2} 1s linear infinite;
+    animation-delay: 0.25s;
+  }
+
+  & > span:nth-child(3) {
+    bottom: 0;
+    right: -100%;
+    width: 100%;
+    height: 2px;
+    background: linear-gradient(270deg, transparent, #03e9f4);
+    animation: ${btnAnimation3} 1s linear infinite;
+    animation-delay: 0.5s;
+  }
+
+  & > span:nth-child(4) {
+    bottom: -100%;
+    left: 0;
+    width: 2px;
+    height: 100%;
+    background: linear-gradient(360deg, transparent, #03e9f4);
+    animation: ${btnAnimation4} 1s linear infinite;
+    animation-delay: 0.75s;
+  }
+`;
+
+export default Home;
--- src/styles/GlobalStyles.js
+++ src/styles/GlobalStyles.js
@@ -3,6 +3,16 @@ import reset from 'styled-reset';
 
 const GlobalStyles = createGlobalStyle`
     ${reset}
+    html {
+      height: 100%;
+    }
+    body {
+      margin:0;
+      padding:0;
+      font-family: sans-serif;
+      background: rgb(34,193,195);
+background: linear-gradient(0deg, rgba(34,193,195,1) 0%, rgba(253,187,45,1) 100%);
+    }
     a{
         text-decoration:none;
         color:inherit
