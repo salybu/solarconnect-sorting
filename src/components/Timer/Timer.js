3b91e36278a1991c6cf6022c00c02ddde9731baa
Jiyeol Jake Lee <jake.lee4006@gmail.com>
2021-08-17 23:39:30 +0900

    [Refactor] 타이머 관련 각종 리팩토링
    - 타이머 숫자 출력 관련 함수 utils 폴더에 따로 모듈화
    - 불필요 변수 및 콘솔 로그 삭제

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -2,6 +2,8 @@ import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
 
+import twoDigitsForTimer from 'utils/twoDigitsForTimer';
+
 const Timer = ({ locale }) => {
   const [time, setTime] = useState('');
   const [date, setDate] = useState('');
@@ -13,44 +15,35 @@ const Timer = ({ locale }) => {
       const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', timeZoneName: 'long' };
 
       if (locale === 'en-US') {
-        // 미국 동부 표준시로 타임존 세팅.
+        // 미국 동부 표준시(뉴욕)로 타임존 세팅.
         options.timeZone = 'America/New_York';
 
         const nyDate = time.toLocaleDateString(locale, options);
 
-        const unixTime = time.getTime();
         let nyTime;
 
         if (nyDate.includes('Daylight')) {
           // 서머타임이 적용되는 경우 북미 동부 표준시는 한국 표준시보다 13시간 느리다.
-          nyTime = new Date(unixTime - 13 * 60 * 60 * 1000);
+          nyTime = new Date(time.getTime() - 13 * 60 * 60 * 1000);
         } else {
           // 일반적인 북미 동부 표준시는 한국 표준시보다 14시간 느리다.
-          nyTime = new Date(unixTime - 14 * 60 * 60 * 1000);
+          nyTime = new Date(time.getTime() - 14 * 60 * 60 * 1000);
         }
-        console.log(nyTime.toLocaleDateString(locale, options));
+
         const hours = nyTime.getHours();
         const minutes = nyTime.getMinutes();
         const seconds = nyTime.getSeconds();
 
-        setTime(`${harold(hours)}:${harold(minutes)}:${harold(seconds)}`);
+        setTime(`${twoDigitsForTimer(hours)}:${twoDigitsForTimer(minutes)}:${twoDigitsForTimer(seconds)}`);
       } else if (locale === 'ko-KR') {
         const hours = time.getHours();
         const minutes = time.getMinutes();
         const seconds = time.getSeconds();
 
-        setTime(`${harold(hours)}:${harold(minutes)}:${harold(seconds)}`);
+        setTime(`${twoDigitsForTimer(hours)}:${twoDigitsForTimer(minutes)}:${twoDigitsForTimer(seconds)}`);
       }
 
       setDate(time.toLocaleDateString(locale, options));
-
-      // TODO : 함수명 바꿀 필요 있음..
-      function harold(standIn) {
-        if (standIn < 10) {
-          standIn = '0' + standIn;
-        }
-        return standIn;
-      }
     };
 
     setInterval(clock, 1000);
